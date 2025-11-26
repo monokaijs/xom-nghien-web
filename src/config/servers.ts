cfcab136e7b58e0062ad2e21286d8bd44170bd0e
monokaijs <monokaijs@gmail.com>
2025-11-26 17:13:24 +0700

    fix: refactor layout

--- src/config/servers.ts
+++ src/config/servers.ts
@@ -4,18 +4,18 @@ export const GameServers: GameServer[] = [{
     id: 'server1',
     name: '[#1] Competitive',
     type: 'counterstrike2',
-    ip: '222.252.30.216',
+    ip: 'cs2.xomnghien.com',
     port: 27015,
 }, {
     id: 'server2',
     name: '[#2] Death Match',
     type: 'counterstrike2',
-    ip: '222.252.30.216',
+    ip: 'cs2.xomnghien.com',
     port: 27021,
 }, {
     id: 'server3',
     name: '[#3] 1v1',
     type: 'counterstrike2',
-    ip: '222.252.30.216',
+    ip: 'cs2.xomnghien.com',
     port: 27025,
 }]

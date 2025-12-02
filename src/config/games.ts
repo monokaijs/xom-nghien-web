6a3a8cd232d0be472ec8fc1383ca35e7271cac10
monokaijs <monokaijs@gmail.com>
2025-12-02 17:51:28 +0700

    feat: further image optimization

--- src/config/games.ts
+++ src/config/games.ts
@@ -1,13 +1,13 @@
 export const Games = [{
   id: 'cs2',
   name: 'Counter-Strike 2',
-  image: '/logos/cs2.png',
+  image: '/logos/cs2.webp',
 }, {
   id: 'aoe_de',
   name: 'AOE: Definitive Edition',
-  image: '/logos/aoe_de.png',
+  image: '/logos/aoe_de.webp',
 }, {
   id: 'lol',
   name: 'League of Legends',
-  image: '/logos/lol.png',
+  image: '/logos/lol.webp',
 }];
